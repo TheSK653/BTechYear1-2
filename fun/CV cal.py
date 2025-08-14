b59d7e603f6709d69d59fc3d423dab3e9486949e
Soham Kundu <153625511+TheSK653@users.noreply.github.com>
2025-08-14 22:12:03 +0530

    Add files via upload

---
+++ fun/CV cal.py
@@ -0,0 +1,7 @@
+print('Artifact-')
+while True:
+    CR=float(input('CR =>'))
+    CD=float(input('CD =>'))
+    print('CV =>',CR*2+CD)
+    if input('------x------\n')!='':
+        break
---
+++ fun/Crit DPS cal.py
@@ -0,0 +1,7 @@
+print('Charecter-')
+while True:
+    CR=float(input('CR =>'))/100
+    CD=float(input('CD =>'))/100
+    print('DPS => ',CR*CD)
+    if input('------x------\n')!='':
+        break
